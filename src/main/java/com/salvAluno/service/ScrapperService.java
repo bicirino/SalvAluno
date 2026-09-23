26f436b00bc122c40e80949a4c784311036c6b37
Gabriel Martins Cirino <gabrielcirinom@gmail.com>
2026-09-23 15:05:26 -0300

    Inicia implementação do método de Scrapping

--- src/main/java/com/salvAluno/service/ScrapperService.java
+++ src/main/java/com/salvAluno/service/ScrapperService.java
@@ -18,6 +18,56 @@ import java.time.LocalDateTime;
 import java.util.ArrayList; 
 import java.util.List; 
 
-// A tag @Service indica que esta classe é um serviço do Spring, permitindo que seja injetada em outras partes da aplicação. 
+// A tag @Service indica que esta classe é um serviço do Spring, permitindo que seja injetada em outras partes da aplicação.
+// O Spring vai instanciar essa classe como um "Bean" que é um objeto especial que é mantido e gerido pelo próprio Spring
 @Service 
+public class ScrapperService { 
+
+    // Injeta o repositório para podermos guardar as tarefas diretamente no banco de dados 
+    @Autowired 
+    private TaskRepository taskRepository; 
+
+    // Injeta a URL do portal da faculdade, que será usada para fazer o scrapping 
+    @Value("${portal.url:https://ea.uniceub.br/Sistema/Acesso/Login}")
+    private String portalUrl; 
+
+    // Injeta o RA do usuário que será usado no login do portal da faculdade 
+    @Value("${portal.RA: seu_RA}") 
+    private String RA; 
+
+    // Injeta a senha do usuário que será usada no login do portal da faculdade 
+    @Value("${portal.password: sua_senha}")
+    private String password; 
+
+    // Método principal para o Scrapping 
+    public void scrapData(){ 
+        System.out.println("Iniciando o scrapping de dados...");
+
+        try (Playwright playwright = Playwright.create()) { 
+
+            // Cria instância Chromium do Playwright   
+            Browser browser = playwright.chromium().launch(
+                // Lança em modo headless (sem interface gráfica) para que o scrapping seja feito em segundo plano 
+                new BrowserType.LaunchOptions().setHeadless(true)
+            );
+            
+            // Cria uma nova aba no navegador para fazer o scrapping 
+            Page page = browser.newPage(); 
+
+            try { 
+
+                System.out.println("[Playwright] Acessando a página de login: " + portalUrl);
+                page.navigate(portalUrl); 
+
+                page.fill("RA ou Email ou CPF", RA); 
+                page.fill("Senha", password); 
+            }catch (Exception e) { 
+                System.err.println("Erro ao preencher os campos de login: " + e.getMessage()); 
+            }
+
+        } catch (Exception e) { 
+            System.err.println("Erro ao iniciar o Playwright: " + e.getMessage()); 
+        }
+    }
+}
 
